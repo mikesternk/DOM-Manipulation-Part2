f2eb3e2b27485dc75428af02f2f0e7a6136e5e5c
mikesternk <mikesternk@gmail.com>
2024-02-23 15:15:34 -0600

    Part 4

--- index.js
+++ index.js
@@ -22,11 +22,35 @@ topMenuEl.classList = "flex-around";
 
 // Part 3
 // Menu data structure
+// DOM MANIPULATION: PART TWO (Part 4)
 var menuLinks = [
   { text: "about", href: "/about" },
-  { text: "catalog", href: "/catalog" },
-  { text: "orders", href: "/orders" },
-  { text: "account", href: "/account" },
+  {
+    text: "catalog",
+    href: "#",
+    subLinks: [
+      { text: "all", href: "/catalog/all" },
+      { text: "top selling", href: "/catalog/top" },
+      { text: "search", href: "/catalog/search" },
+    ],
+  },
+  {
+    text: "orders",
+    href: "#",
+    subLinks: [
+      { text: "new", href: "/orders/new" },
+      { text: "pending", href: "/orders/pending" },
+      { text: "history", href: "/orders/history" },
+    ],
+  },
+  {
+    text: "account",
+    href: "#",
+    subLinks: [
+      { text: "profile", href: "/account/profile" },
+      { text: "sign out", href: "/account/signout" },
+    ],
+  },
 ];
 
 menuLinks.forEach((link) => {
@@ -51,4 +75,30 @@ subMenuEl.classList = "flex-around";
 
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
-// subMenuEl.style.zIndex = "100";
+// subMenuEl.style.zIndex = "100"; // just to make sure its actually there lol
+
+// Part 4
+let topMenuLinks = document.querySelectorAll("a");
+console.log(topMenuLinks);
+
+topMenuEl.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  if (!event.target.matches("a")) return;
+  // if (event.target.tagName != "A") return;
+
+  console.log(event.target.textContent);
+
+  // Check if the clicked link is already active
+  const isAlreadyActive = event.target.classList.contains("active");
+
+  // removes "active" class from all links
+  topMenuLinks.forEach((link) => {
+    link.classList.remove("active");
+  });
+  if (!isAlreadyActive) {
+    event.target.classList.toggle("active");
+  }
+  // event.target.classList.add("active");
+  // event.target.classList.toggle("active");
+});
