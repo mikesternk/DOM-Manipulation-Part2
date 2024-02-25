082e0d52bb6c1649b25d94f8ba828792a80931d5
mikesternk <mikesternk@gmail.com>
2024-02-25 14:09:48 -0600

    Part 5: Progress Check

--- index.js
+++ index.js
@@ -98,7 +98,65 @@ topMenuEl.addEventListener("click", (event) => {
   });
   if (!isAlreadyActive) {
     event.target.classList.toggle("active");
+
+    // Get the text content of the clicked link
+    const clickedLinkText = event.target.textContent;
+
+    // Find the corresponding link object from menuLinks based on the text content
+    const clickedLinkObject = menuLinks.find(
+      (link) => link.text === clickedLinkText
+    );
+
+    // Check if the link object has subLinks and the text content is not "about"
+    if (
+      clickedLinkObject &&
+      clickedLinkObject.subLinks &&
+      clickedLinkText !== "about"
+    ) {
+      // Cache the "subLinks" array for later use
+      const subLinksArray = clickedLinkObject.subLinks;
+      buildSubmenu(subLinksArray);
+
+      subMenuEl.style.top = "100%";
+    } else {
+      subMenuEl.style.top = "0";
+    }
+  } else {
+    // Clear the contents of subMenuEl and set its top position to 0 when the active link is clicked again
+    subMenuEl.innerHTML = "";
+    subMenuEl.style.top = "0";
   }
+
   // event.target.classList.add("active");
   // event.target.classList.toggle("active");
+
+  // Part 5
+  // Get the corresponding link object from menuLinks
+  // const clickedLinkObject = menuLinks.find(
+  //   (link) => link.text === event.target.innerText
+  // );
 });
+
+// creating the helper function
+const buildSubmenu = (subLinksArray) => {
+  console.log("Received subLinksArray:", subLinksArray);
+
+  // Clear the current contents of subMenuEl.
+  subMenuEl.innerHTML = "";
+
+  // Check if subLinksArray is defined
+  if (subLinksArray && Array.isArray(subLinksArray)) {
+    subLinksArray.forEach((link) => {
+      // creating an <a> element, and assigning to a variable
+      let subLinkElement = document.createElement("a");
+
+      // adding a href attribute
+      subLinkElement.href = link.href;
+
+      // Set the element's content to the value of the text property of the "link" object
+      subLinkElement.innerText = link.text;
+
+      subMenuEl.appendChild(subLinkElement);
+    });
+  }
+};
